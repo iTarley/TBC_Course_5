b07cc1a12079ed0f97ade6257fae6bceaed933ac
Luka <nikuradzeluka0@gmail.com>
2022-06-25 13:16:57 +0400

    commit

---
+++ app/src/main/java/com/example/tbc_course_5/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.tbc_course_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
